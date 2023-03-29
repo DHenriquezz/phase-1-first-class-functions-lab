e194cc8138654ea920d3dc34e8cf478c7d9acbc5
Douglas Flores <Dflores5df@icloud.com>
2023-03-29 14:12:46 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+     const scubers = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+return scubers.splice(0,2)
+}
+const returnLastTwoDrivers = function(){
+    const scubers = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+return scubers.splice(2,3)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+    return function(ride){
+        return (fare * ride)
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+const scubers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const  selectDifferentDrivers = (scubers, cb) =>{
+  return cb(returnFirstTwoDrivers(scubers))
+
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -13,7 +13,7 @@ describe('index.js', function () {
     });
   });
 
-  describe('returnLastTwoDrivers()', function () {
+  describe('returnLastTwoDrivers', function () {
     it('should return an array of the last two drivers', function () {
       expect(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])).to.eql(['Amari', 'Mo']);
     });
